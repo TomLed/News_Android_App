adb22afa568ff7352ff13dd7200644af64682ba8
Thomas <thomas.ledar@centrale-marseille.fr>
2019-12-02 23:49:26 +0100

    Retrieve Data from News Api

--- app/src/main/java/com/example/yourdailydigest/MainActivity.java
+++ app/src/main/java/com/example/yourdailydigest/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.yourdailydigest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.yourdailydigest.Model.Articles;
+import com.example.yourdailydigest.Model.Headlines;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    final String API_KEY = "d5e1f0d930974389b567801f96aa2bed";
+    Adapter adapter;
+    List<Articles> articles = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        String country = getCountry();
+        retrieveJson(country, API_KEY);
+
+    }
+
+    public void retrieveJson(String country, String apiKey){
+
+        Call<Headlines> call = ApiCLient.getInstance().getApi().getHeadlines(country, apiKey);
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+                if (response.isSuccessful() && response.body().getArticles() != null){
+                    articles.clear();
+                    articles = response.body().getArticles();
+                    adapter = new Adapter(MainActivity.this,articles);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    public String getCountry(){
+        Locale locale = Locale.getDefault();
+        String country = locale.getCountry();
+        return country.toLowerCase();
     }
 }
